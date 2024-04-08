168fb4a93dadca928ac98349fda13d46251cc0ab
Natasha Limeres <natashaslimeres@gmail.com>
2024-04-08 18:33:44 -0300

    Updating banner image and adding phone number/free freight information

--- src/pages/components/header.tsx
+++ src/pages/components/header.tsx
@@ -4,6 +4,7 @@ import {
   Search,
   ShoppingCart,
   MessageSquareHeartIcon,
+  Phone,
 } from "lucide-react";
 import Image from "next/image";
 
@@ -29,58 +30,67 @@ export default function Header() {
   }, []);
 
   return (
-    <header className="bg-white h-36 text-mainGray p-8 flex justify-between">
-      <div className="w-72">
-        <Image
-          src="/transparent-logo2.png"
-          alt="Imagem do Banner"
-          width={2500}
-          height={400}
-          className="rounded-lg"
-        />
+    <>
+      <div className="flex items-center justify-between bg-gray-200 text-mainGray py-2 px-4">
+        <div className="flex items-center">
+          <Phone size="20" strokeWidth="2" className="mr-1" />
+          <p>(00) 0000-0000</p>
+        </div>
+        <p>FRETE GRÁTIS ACIMA DE R$200</p>
       </div>
+      <header className="bg-white h-36 text-mainGray p-8 flex justify-between">
+        <div className="w-72">
+          <Image
+            src="/transparent-logo2.png"
+            alt="Imagem do Banner"
+            width={2500}
+            height={400}
+            className="rounded-lg"
+          />
+        </div>
 
-      <div className="relative mx-auto text-mainGreen text-lg">
-        <input
-          className="border-2 border-secondaryGreen bg-lightGray h-12 pl-14 pr-28 rounded-full text-lg focus:outline-none"
-          type="text"
-          name="text"
-          placeholder="Pesquisar"
-        />
-        <Search className="absolute cursor-pointer right-4 top-3" size="22" />
-      </div>
+        <div className="relative mx-auto text-mainGreen text-lg">
+          <input
+            className="border-2 border-secondaryGreen bg-lightGray h-12 pl-14 pr-28 rounded-full text-lg focus:outline-none"
+            type="text"
+            name="text"
+            placeholder="Pesquisar"
+          />
+          <Search className="absolute cursor-pointer right-4 top-3" size="22" />
+        </div>
 
-      <div className="cursor-pointer pr-7 text-mainGreen">
-        <MessageSquareHeartIcon size="32" strokeWidth="1" />
-      </div>
+        <div className="cursor-pointer pr-7 text-mainGreen">
+          <MessageSquareHeartIcon size="32" strokeWidth="1" />
+        </div>
 
-      <div className="cursor-pointer pr-7 text-mainGreen">
-        <ShoppingCart size="32" strokeWidth="1" />
-      </div>
+        <div className="cursor-pointer pr-7 text-mainGreen">
+          <ShoppingCart size="32" strokeWidth="1" />
+        </div>
 
-      <div className="cursor-pointer text-mainGreen" onClick={toggleMenu}>
-        <Menu size="32" strokeWidth="1" />
-      </div>
+        <div className="cursor-pointer text-mainGreen" onClick={toggleMenu}>
+          <Menu size="32" strokeWidth="1" />
+        </div>
 
-      {isMenuOpen && (
-        <nav
-          ref={menuRef}
-          className="absolute top-16 right-2 mt-2 bg-mainGreen text-white text-xl z-50 w-32 p-2 rounded"
-        >
-          <a href="#" className="block py-1">
-            Inicio
-          </a>
-          <a href="#" className="block py-1">
-            Produtos
-          </a>
-          <a href="#" className="block py-1">
-            Contato
-          </a>
-          <a href="#" className="block py-1">
-            Promoções
-          </a>
-        </nav>
-      )}
-    </header>
+        {isMenuOpen && (
+          <nav
+            ref={menuRef}
+            className="absolute top-16 right-2 mt-2 bg-mainGreen text-white text-xl z-50 w-32 p-2 rounded"
+          >
+            <a href="#" className="block py-1">
+              Inicio
+            </a>
+            <a href="#" className="block py-1">
+              Produtos
+            </a>
+            <a href="#" className="block py-1">
+              Contato
+            </a>
+            <a href="#" className="block py-1">
+              Promoções
+            </a>
+          </nav>
+        )}
+      </header>
+    </>
   );
 }
